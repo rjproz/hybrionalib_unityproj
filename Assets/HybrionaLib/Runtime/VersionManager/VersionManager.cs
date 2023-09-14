bc36d0b7a276ded7362f057cb673dc0f3e55aaa9
Rajkumar Pramanik "RJproz <rjproz@gmail.com>
2023-09-15 01:27:26 +0530

    no message

---
+++ Assets/HybrionaLib/Runtime/VersionManager/VersionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hybriona
+{
+	public class VersionManager  
+	{
+		private static bool isVersionFetched = false;
+		private static AppVersion version;
+		public static AppVersion Version()
+		{
+			if(!isVersionFetched || !Application.isPlaying)
+			{
+				TextAsset textAsset = Resources.Load<TextAsset>("app_version");
+				if(textAsset == null)
+				{
+					version = new AppVersion();
+				}
+				else
+				{
+					version = JsonUtility.FromJson<AppVersion>(textAsset.text);
+				}
+				isVersionFetched = true;
+			}
+			return version;
+
+		}
+	}
+}
---
+++ Assets/HybrionaLib/Runtime/VersionManager/VersionToText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Hybriona
+{
+	public class VersionToText : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			GetComponent<Text>().text= VersionManager.Version().ToString();
+		}
+		
+
+	}
+}
