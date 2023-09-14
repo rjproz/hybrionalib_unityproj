7c7a724e59cff656be94986bfe789abce2ac117f
Rajkumar Pramanik "RJproz <rjproz@gmail.com>
2023-09-14 21:23:39 +0530

    - added http rest api

---
+++ Assets/HybrionaLib/Runtime/Core/HybSingleton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HybSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+
+	private static T m_instance;
+	public static T Instance
+	{
+		get
+		{
+			if(m_instance == null)
+			{
+				m_instance = GameObject.FindObjectOfType<T>();
+				if(m_instance == null)
+				{
+					Debug.Log("Creating new instance of "+typeof(T).ToString());
+					GameObject o = new GameObject(typeof(T).ToString());
+					m_instance = o.AddComponent<T>();
+				}
+			}
+			return m_instance;
+		}
+	}
+
+	public void SetDontDestroyOnLoad()
+	{
+		DontDestroyOnLoad (Instance.gameObject);
+	}
+
+}
---
+++ Assets/HybrionaLib/Runtime/HttpRest/HybHttpResponse.cs
@@ -0,0 +1,65 @@
+﻿#if HYBRIONA_LIB_ENABLE_HTTP_CLIENT
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+namespace Hybriona
+{
+	public struct HttpResponse
+	{
+		public int status { get; private set;}
+
+		public UnityWebRequest webRequest {get;private set;}
+
+		public Dictionary<string,string> responseHeaders { get; private set;}
+		public ulong downloadedBytes { get; private set;}
+		public byte [] bytes { get; private set;}
+		public string text { get; private set;}
+
+		public string error { get; private set;}
+		public bool isHttpError { get; private set;}
+		public bool isNetworkError { get; private set;}
+
+		public float time {get;private set;}
+
+		public enum ContentType {Unknown,Json};
+
+		public HttpResponse(UnityWebRequest request,float timeTaken)
+		{
+			if(!request.isDone)
+			{
+				throw new System.Exception("HttpResponse shouldn't call before UnityWebRequest finishes!");
+			}
+			webRequest = request;
+			time = timeTaken;
+
+			status = (int) request.responseCode;
+
+			downloadedBytes = request.downloadedBytes;
+			bytes = request.downloadHandler.data;
+			text = request.downloadHandler.text;
+			error = request.error;
+
+			isHttpError = request.isHttpError;
+			isNetworkError = request.isNetworkError;
+
+			responseHeaders = request.GetResponseHeaders();
+
+		}
+
+		public ContentType GetContentType()
+		{
+			if(responseHeaders!= null && responseHeaders.ContainsKey("Content-type"))
+			{
+				string type = responseHeaders["Content-type"];
+				if(type == "application/json")
+				{
+					return ContentType.Json;
+				}
+
+			}
+			return ContentType.Unknown;
+		}
+	}
+}
+#endif
