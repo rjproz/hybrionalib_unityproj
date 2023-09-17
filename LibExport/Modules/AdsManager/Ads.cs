23db1f09b055651f42c587411c4fe592b37577f9
Rajkumar Pramanik "RJproz <rjproz@gmail.com>
2023-09-17 22:42:23 +0530

    no message

--- LibExport/Modules/AdsManager/Ads.cs
+++ LibExport/Modules/AdsManager/Ads.cs
@@ -6,11 +6,11 @@ namespace Hybriona
     public class AdUnit
     {
         public string identifier { get; private set; }
-        public AdSource source { get; private set; }
         public AdType type { get; private set; }
-        public string ios_unitID { get; private set; }
-        public string android_unitId { get; private set; }
-       
+        
+
+        public string unitId { get; private set; }
+
         public System.Action onAdLoaded { get;  set; }
         public System.Action onAdFailedToLoad { get; set; }
         public System.Action onAdClosed { get; set; }
@@ -23,27 +23,20 @@ namespace Hybriona
         private bool m_IsAdAvailable;
         private bool m_IsAdTryingToLoad;
 
-        public AdUnit(AdSource source, AdType type, string ios_unitID, string android_unitId)
+       
+
+        public AdUnit(AdType type, string unitId)
         {
             this.identifier = Random.Range(100000, 999999) + "_" + Random.Range(100000, 999999);
-            this.source = source;
             this.type = type;
-            this.ios_unitID = ios_unitID;
-            this.android_unitId = android_unitId;
-            
+            this.unitId = unitId;
+           
+
         }
 
         public string GetUnitId()
         {
-            if(Application.platform == RuntimePlatform.Android)
-            {
-                return android_unitId;
-            }
-            else if(Application.platform == RuntimePlatform.IPhonePlayer)
-            {
-                return ios_unitID;
-            }
-            return null;
+            return unitId;
         }
 
         public void LoadAd()
@@ -54,81 +47,81 @@ namespace Hybriona
             }
 
            
-            if (source == AdSource.AdMob)
+           
+            
+            if(type == AdType.Interstitial)
             {
-                if(type == AdType.Interstitial)
+                if (interstitialAd != null)
                 {
-                    if (interstitialAd != null)
-                    {
-                        interstitialAd.Destroy();
-                        interstitialAd = null;
-                    }
-
-                    m_IsAdTryingToLoad = true;
-                    var adRequest = new AdRequest();
-                    InterstitialAd.Load(GetUnitId(), adRequest,
-                      (InterstitialAd ad, LoadAdError error) =>
-                      {
-                          m_IsAdTryingToLoad = false;
-                          // if error is not null, the load request failed.
-                          if (error != null || ad == null)
-                          {
-
-
-                              //error event
-
-                              InvokeLoadErrorEvent();
-                              return;
-                          }
-
-                          Debug.Log("Interstitial ad loaded with response : "
-                                    + ad.GetResponseInfo());
-                          m_IsAdAvailable = true;
-                          interstitialAd = ad;
-
-                          InvokeAdLoadedEvent();
-                      });
-
+                    interstitialAd.Destroy();
+                    interstitialAd = null;
                 }
-                else if(type == AdType.Rewarded)
-                {
 
-                    if (rewardedAd != null)
+                m_IsAdTryingToLoad = true;
+                var adRequest = new AdRequest();
+                InterstitialAd.Load(GetUnitId(), adRequest,
+                    (InterstitialAd ad, LoadAdError error) =>
                     {
-                        rewardedAd.Destroy();
-                        rewardedAd = null;
-                    }
-
-                    Debug.Log("Loading the rewarded ad.");
-
-
-                    m_IsAdTryingToLoad = true;
-
-                    // create our request used to load the ad.
-                    var adRequest = new AdRequest();
-                  
-                    // send the request to load the ad.
-                    RewardedAd.Load(GetUnitId(), adRequest,(RewardedAd ad, LoadAdError error) =>{
                         m_IsAdTryingToLoad = false;
+                        // if error is not null, the load request failed.
                         if (error != null || ad == null)
                         {
-                            Debug.LogError("Rewarded ad failed to load an ad " +
-                                            "with error : " + error);
+
+
+                            //error event
 
                             InvokeLoadErrorEvent();
                             return;
                         }
 
-                        Debug.Log("Rewarded ad loaded with response : "
-                                    + ad.GetResponseInfo());
-
+                        Debug.Log("Interstitial ad loaded with response : "
+                                + ad.GetResponseInfo());
                         m_IsAdAvailable = true;
-                        rewardedAd = ad;
+                        interstitialAd = ad;
+
                         InvokeAdLoadedEvent();
                     });
 
-                }
             }
+            else if(type == AdType.Rewarded)
+            {
+
+                if (rewardedAd != null)
+                {
+                    rewardedAd.Destroy();
+                    rewardedAd = null;
+                }
+
+                Debug.Log("Loading the rewarded ad.");
+
+
+                m_IsAdTryingToLoad = true;
+
+                // create our request used to load the ad.
+                var adRequest = new AdRequest();
+                  
+                // send the request to load the ad.
+                RewardedAd.Load(GetUnitId(), adRequest,(RewardedAd ad, LoadAdError error) =>{
+                    m_IsAdTryingToLoad = false;
+                    if (error != null || ad == null)
+                    {
+                        Debug.LogError("Rewarded ad failed to load an ad " +
+                                        "with error : " + error);
+
+                        InvokeLoadErrorEvent();
+                        return;
+                    }
+
+                    Debug.Log("Rewarded ad loaded with response : "
+                                + ad.GetResponseInfo());
+
+                    m_IsAdAvailable = true;
+                    rewardedAd = ad;
+                    InvokeAdLoadedEvent();
+                });
+
+            }
+            
         }
 
         public bool IsAdAvailable()
@@ -141,95 +134,94 @@ namespace Hybriona
         public void ShowAd()
         {
             m_IsAdAvailable = false;
-            if (source == AdSource.AdMob)
+          
+            if (type == AdType.Interstitial)
             {
-                if (type == AdType.Interstitial)
+                var ad = interstitialAd;
+                // Raised when the ad is estimated to have earned money.
+                ad.OnAdPaid += (AdValue adValue) =>
                 {
-                    var ad = interstitialAd;
-                    // Raised when the ad is estimated to have earned money.
-                    ad.OnAdPaid += (AdValue adValue) =>
-                    {
                         
-                        //Debug.Log(String.Format("Interstitial ad paid {0} {1}.",adValue.Value,adValue.CurrencyCode));
-                    };
-                    // Raised when an impression is recorded for an ad.
-                    ad.OnAdImpressionRecorded += () =>
-                    {
-                        Debug.Log("Interstitial ad recorded an impression.");
-                    };
-                    // Raised when a click is recorded for an ad.
-                    ad.OnAdClicked += () =>
-                    {
-                        Debug.Log("Interstitial ad was clicked.");
-                    };
-                    // Raised when an ad opened full screen content.
-                    ad.OnAdFullScreenContentOpened += () =>
-                    {
-                        Debug.Log("Interstitial ad full screen content opened.");
-                    };
-                    // Raised when the ad closed full screen content.
-                    ad.OnAdFullScreenContentClosed += () =>
-                    {
-                        Debug.Log("Interstitial ad full screen content closed.");
-                        InvokeAdClosedEvent();
-
-                    };
-                    // Raised when the ad failed to open full screen content.
-                    ad.OnAdFullScreenContentFailed += (AdError error) =>
-                    {
-                        Debug.LogError("Interstitial ad failed to open full screen content " +
-                                       "with error : " + error);
-                        InvokeAdClosedEvent();
-                    };
-
-                    interstitialAd.Show();
-
-                }
-                else if(type == AdType.Rewarded)
+                    //Debug.Log(String.Format("Interstitial ad paid {0} {1}.",adValue.Value,adValue.CurrencyCode));
+                };
+                // Raised when an impression is recorded for an ad.
+                ad.OnAdImpressionRecorded += () =>
                 {
-                    var ad = rewardedAd;
-                    // Raised when the ad is estimated to have earned money.
-                    ad.OnAdPaid += (AdValue adValue) =>
-                    {
+                    Debug.Log("Interstitial ad recorded an impression.");
+                };
+                // Raised when a click is recorded for an ad.
+                ad.OnAdClicked += () =>
+                {
+                    Debug.Log("Interstitial ad was clicked.");
+                };
+                // Raised when an ad opened full screen content.
+                ad.OnAdFullScreenContentOpened += () =>
+                {
+                    Debug.Log("Interstitial ad full screen content opened.");
+                };
+                // Raised when the ad closed full screen content.
+                ad.OnAdFullScreenContentClosed += () =>
+                {
+                    Debug.Log("Interstitial ad full screen content closed.");
+                    InvokeAdClosedEvent();
 
-                        //Debug.Log(String.Format("Interstitial ad paid {0} {1}.",adValue.Value,adValue.CurrencyCode));
-                    };
-                    // Raised when an impression is recorded for an ad.
-                    ad.OnAdImpressionRecorded += () =>
-                    {
-                        Debug.Log("Rewarded ad recorded an impression.");
-                    };
-                    // Raised when a click is recorded for an ad.
-                    ad.OnAdClicked += () =>
-                    {
-                        Debug.Log("Rewarded ad was clicked.");
-                    };
-                    // Raised when an ad opened full screen content.
-                    ad.OnAdFullScreenContentOpened += () =>
-                    {
-                        Debug.Log("Rewarded ad full screen content opened.");
-                    };
-                    // Raised when the ad closed full screen content.
-                    ad.OnAdFullScreenContentClosed += () =>
-                    {
-                       
-                        Debug.Log("Rewarded ad full screen content closed.");
-                        InvokeAdClosedEvent();
-                    };
-                    // Raised when the ad failed to open full screen content.
-                    ad.OnAdFullScreenContentFailed += (AdError error) =>
-                    {
-                        Debug.LogError("Rewarded ad failed to open full screen content " +
-                                       "with error : " + error);
-                        InvokeAdClosedEvent();
-                    };
+                };
+                // Raised when the ad failed to open full screen content.
+                ad.OnAdFullScreenContentFailed += (AdError error) =>
+                {
+                    Debug.LogError("Interstitial ad failed to open full screen content " +
+                                    "with error : " + error);
+                    InvokeAdClosedEvent();
+                };
 
-                    rewardedAd.Show((reward) =>
-                    {
-                        InvokeRewardGrantedEvent();
-                    });
-                }
+                interstitialAd.Show();
+
             }
+            else if(type == AdType.Rewarded)
+            {
+                var ad = rewardedAd;
+                // Raised when the ad is estimated to have earned money.
+                ad.OnAdPaid += (AdValue adValue) =>
+                {
+
+                    //Debug.Log(String.Format("Interstitial ad paid {0} {1}.",adValue.Value,adValue.CurrencyCode));
+                };
+                // Raised when an impression is recorded for an ad.
+                ad.OnAdImpressionRecorded += () =>
+                {
+                    Debug.Log("Rewarded ad recorded an impression.");
+                };
+                // Raised when a click is recorded for an ad.
+                ad.OnAdClicked += () =>
+                {
+                    Debug.Log("Rewarded ad was clicked.");
+                };
+                // Raised when an ad opened full screen content.
+                ad.OnAdFullScreenContentOpened += () =>
+                {
+                    Debug.Log("Rewarded ad full screen content opened.");
+                };
+                // Raised when the ad closed full screen content.
+                ad.OnAdFullScreenContentClosed += () =>
+                {
+                       
+                    Debug.Log("Rewarded ad full screen content closed.");
+                    InvokeAdClosedEvent();
+                };
+                // Raised when the ad failed to open full screen content.
+                ad.OnAdFullScreenContentFailed += (AdError error) =>
+                {
+                    Debug.LogError("Rewarded ad failed to open full screen content " +
+                                    "with error : " + error);
+                    InvokeAdClosedEvent();
+                };
+
+                rewardedAd.Show((reward) =>
+                {
+                    InvokeRewardGrantedEvent();
+                });
+            }
+            
         }
 
        
