fc4805a620d511755422bc24ee0f00a52dcaebb0
Rajkumar Pramanik "RJproz <rjproz@gmail.com>
2023-09-18 14:32:08 +0530

    no message

--- Assets/HybrionaLib/Modules/Core/Activator/HybrionaUnityLibActivator.cs
+++ Assets/HybrionaLib/Modules/Core/Activator/HybrionaUnityLibActivator.cs
@@ -223,7 +223,7 @@ namespace Hybriona
                             //UnityEditor.PackageManager.Client.Add(pkg.packageId);
 
 
-                            var downloadRequest = UnityWebRequest.Get(PackageDownloadPath+ "/hybriona.unitylib");
+                            var downloadRequest = UnityWebRequest.Get(PackageDownloadPath+ "/hybriona.unitylib.tgz");
                             var downloadOperation = downloadRequest.SendWebRequest();
 
                             while(!downloadOperation.isDone)
