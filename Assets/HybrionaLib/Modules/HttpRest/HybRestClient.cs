3c021158c4f19b02f3c98c2c76e4bdc209e2f16c
Rajkumar Pramanik "RJproz <rjproz@gmail.com>
2024-06-08 11:08:37 +0530

    no message

--- Assets/HybrionaLib/Modules/HttpRest/HybRestClient.cs
+++ Assets/HybrionaLib/Modules/HttpRest/HybRestClient.cs
@@ -1,203 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Networking;
 
 namespace Hybriona
 {
-	public class RestClient : HybSingleton<RestClient> {
+	public class RestClient {
 
-		void Start()
+
+
+		public static async Task<UnityWebRequest> Get(string url,Dictionary<string,string> headers)
+        {
+			var request = UnityWebRequest.Get(url);
+		
+			AddHeadersToRequest(headers, request);
+			return await ProcessRequest(request);
+        }
+
+		public static async Task<UnityWebRequest> Head(string url, Dictionary<string, string> headers)
 		{
-			GameObject.DontDestroyOnLoad(gameObject);
+			var request = UnityWebRequest.Head(url);
+			AddHeadersToRequest(headers, request);
+			return await ProcessRequest(request);
 		}
 
-#region Using WWW
-		/*
-		public delegate void OnComplete(WWW www);
-		public WWW Post(string url,string data,Dictionary<string,string> headers,OnComplete onComplete = null)
+		public static async Task<UnityWebRequest> Delete(string url, Dictionary<string, string> headers)
 		{
-			
-			return Post(url,System.Text.Encoding.ASCII.GetBytes(data),headers,onComplete);
-		}
-		public WWW Post(string url,byte [] data,Dictionary<string,string> headers,OnComplete onComplete= null)
-		{
-			
-			WWW www = null;
-			if(headers == null)
-			{
-				www = new WWW(url,data);
-			}
-			else
-			{
-				www = new WWW(url,data,headers);
-			}
-
-			if(!Application.isEditor || Application.isPlaying)
-			{
-				StartCoroutine(ProcessWWW(www,onComplete));
-
-			}
-			return www;
+			var request = UnityWebRequest.Delete(url);
+			AddHeadersToRequest(headers, request);
+			return await ProcessRequest(request);
 		}
 
-		public WWW Get(string url,Dictionary<string,string> headers,OnComplete onComplete = null)
+		public static async Task<UnityWebRequest> Put(string url,byte [] data, Dictionary<string, string> headers)
 		{
-			WWW www = null;
-			if(headers == null)
-			{
-				www = new WWW(url);
-			}
-			else
-			{
-				www = new WWW(url,null,headers);
-			}
-
-			if(!Application.isEditor || Application.isPlaying)
-			{
-				StartCoroutine(ProcessWWW(www,onComplete));
-
-			}
-			return www;
+			var request = UnityWebRequest.Put(url,data);
+			AddHeadersToRequest(headers, request);
+			return await ProcessRequest(request);
 		}
 
-
-
-		IEnumerator ProcessWWW(WWW www, OnComplete onComplete)
+		public static async Task<UnityWebRequest> Post(string url, string data, Dictionary<string, string> headers)
 		{
-			yield return www;
-			onComplete(www);
+			var request = UnityWebRequest.Post(url, data);
+			AddHeadersToRequest(headers, request);
+			return await ProcessRequest(request);
 		}
-		*/
-#endregion
 
-#region using UnityWebrequest
-
-		public delegate void OnCompleteRequest(HttpResponse response);
-
-		public void Process(string url, WWWForm form, Dictionary<string, string> headers = null, OnCompleteRequest onComplete = null)
+		public static async Task<UnityWebRequest> Post(string url, WWWForm form, Dictionary<string, string> headers)
 		{
-			UnityWebRequest request =  UnityWebRequest.Post(url,form);
-			foreach(var keyval in headers)
+			var request = UnityWebRequest.Post(url, form);
+			AddHeadersToRequest(headers, request);
+			return await ProcessRequest(request);
+		}
+
+		public static async Task<UnityWebRequest> ProcessRequest(UnityWebRequest webRequest)
+		{
+			webRequest.disposeDownloadHandlerOnDispose = true;
+			webRequest.disposeUploadHandlerOnDispose = true;
+			var operation = webRequest.SendWebRequest();
+			while(!operation.isDone)
             {
-				request.SetRequestHeader(keyval.Key, keyval.Value);
+				await Task.Yield();
             }
-			var operation = request.SendWebRequest();
-			if (!Application.isEditor || Application.isPlaying)
+			return webRequest;
+		}
+
+		private static void AddHeadersToRequest(Dictionary<string,string> headers,UnityWebRequest request)
+        {
+			if (headers != null)
 			{
-				StartCoroutine(ProcessWebRequest(operation, onComplete));
+				foreach (var header in headers)
+                {
+					request.SetRequestHeader(header.Key, header.Value);
+                }
 			}
-			else
-			{
-
-				while (!operation.isDone)
-				{
-
-				}
-				if (onComplete != null)
-				{
-					onComplete(new HttpResponse(operation.webRequest, -1));
-				}
-				operation.webRequest.Dispose();
-
-			}
-
-
-
-		}
-		public void Process(string url,string method,byte [] data,Dictionary<string,string> headers = null,OnCompleteRequest onComplete = null)
-		{
-			UnityWebRequest request = new UnityWebRequest();
-
-			
-			request.disposeDownloadHandlerOnDispose = true;
-			request.disposeUploadHandlerOnDispose = true;
-
-			request.url = url;
-			request.method = method;
-
-
-			if(data != null && data.Length > 0)
-			{
-				request.uploadHandler = new UploadHandlerRaw(data);
-				
-			}
-			if(headers != null)
-			{
-				foreach(KeyValuePair<string,string> header in headers)
-				{
-					request.SetRequestHeader(header.Key,header.Value);
-				}
-			}
-			request.downloadHandler = new DownloadHandlerBuffer();
-
-			UnityWebRequestAsyncOperation operation = request.SendWebRequest();
-			if(!Application.isEditor || Application.isPlaying)
-			{
-				StartCoroutine(ProcessWebRequest(operation,onComplete));
-			}
-			else
-			{
-				
-				while(!operation.isDone)
-				{
-					
-				}
-				if(onComplete != null)
-				{
-					onComplete(new HttpResponse( operation.webRequest , -1) );
-				}
-				operation.webRequest.Dispose();
-
-			}
-				
-
-
-		}
-		IEnumerator ProcessWebRequest(UnityWebRequestAsyncOperation operation, OnCompleteRequest onComplete)
-		{
-			float timeStarted = Time.time;
-			yield return operation;
-
-			if(onComplete != null)
-			{
-				onComplete(new HttpResponse( operation.webRequest , Time.time - timeStarted ) );
-			}
-			operation.webRequest.Dispose();
 		}
 
-
-		public void Get(string url,Dictionary<string,string> headers = null,OnCompleteRequest onComplete = null)
-		{
-			Process(url,UnityWebRequest.kHttpVerbGET,null,headers,onComplete);
-		}
-		public void Delete(string url,Dictionary<string,string> headers = null,OnCompleteRequest onComplete = null)
-		{
-			Process(url,UnityWebRequest.kHttpVerbDELETE,null,headers,onComplete);
-		}
-
-		public void PostForm(string url, WWWForm form, Dictionary<string, string> headers = null, OnCompleteRequest onComplete = null)
-		{
-			Process(url, form, headers, onComplete);
-		}
-
-		public void Post(string url,byte [] data,Dictionary<string,string> headers = null,OnCompleteRequest onComplete = null)
-		{
-			Process(url,UnityWebRequest.kHttpVerbPOST,data,headers,onComplete);
-		}
-
-		public void Put(string url,byte [] data,Dictionary<string,string> headers = null,OnCompleteRequest onComplete = null)
-		{
-			Process(url,UnityWebRequest.kHttpVerbPUT,data,headers,onComplete);
-		}
-
-		public void Head(string url,Dictionary<string,string> headers = null,OnCompleteRequest onComplete = null)
-		{
-			Process(url,UnityWebRequest.kHttpVerbHEAD,null,headers,onComplete);
-		}
-
-
-#endregion
+		
 
 	}
 	public static class RestClientExtensions
@@ -206,5 +85,18 @@ namespace Hybriona
 		{
 			return System.Text.Encoding.UTF8.GetBytes(data);
 		}
+
+		public static async void Callback(this Task<UnityWebRequest> task,System.Action<UnityWebRequest> action)
+        {
+			while(!task.IsCompleted)
+            {
+				await Task.Yield();
+            }
+
+			if(action != null)
+            {
+				action(task.Result);
+            }
+        }
 	}
 }
